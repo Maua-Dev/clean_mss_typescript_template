6c7d9996ec83461cef7dabf17b6799f91e9bd682
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-01-22 14:04:20 -0300

    added new config to envs file✅

--- src/shared/environments.ts
+++ src/shared/environments.ts
@@ -1,4 +1,3 @@
-import { env } from './env'
 import { STAGE } from './domain/enums/stage_enum'
 import { IUserRepository } from './domain/repositories/user_repository_interface'
 import { UserRepositoryDynamo } from './infra/repositories/user_repository_dynamo'
@@ -16,40 +15,51 @@ export class Environments {
   mssName: string = ''
 
   configureLocal() {
-    env.STAGE = env.STAGE || 'TEST'
+    console.log('process.env.STAGE - [ENVIRONMENTS - { CONFIGURE LOCAL }] - ', process.env.STAGE)
+    process.env.STAGE = process.env.STAGE || 'TEST'
   }
 
   loadEnvs() {
-    if (!env.STAGE) {
+    if (!process.env.STAGE) {
       this.configureLocal()
     }
+
     
-    this.stage = env.STAGE as STAGE
-    this.mssName = env.MSS_NAME as string
+    this.stage = process.env.STAGE as STAGE
+
+    console.log('process.env.STAGE - [CHEGOU NO LOAD_ENVS] - ', process.env.STAGE)
+    console.log('process.env.DYNAMOTABLENAME - [CHEGOU NO LOAD_ENVS] - ', process.env.DYNAMO_TABLE_NAME)
+    console.log('process.env.ENDPOINT_URL - [CHEGOU NO LOAD_ENVS] - ', process.env.ENDPOINT_URL)
+    console.log('process.env.REGION - [CHEGOU NO LOAD_ENVS] - ', process.env.REGION)
+    console.log('this.stage - [CHEGOU NO LOAD_ENVS] - ', this.stage)
+    console.log('this.DYNAMOTABLENAME - [CHEGOU NO LOAD_ENVS] - ', this.dynamoTableName)
+    this.mssName = process.env.MSS_NAME as string
 
     if (this.stage === STAGE.TEST) {
       this.s3BucketName = 'bucket-test'
       this.region = 'sa-east-1'
       this.endpointUrl = 'http://localhost:8000'
-      this.dynamoTableName = 'user_mss_template-table'
+      this.dynamoTableName = 'UserMssTemplateTable'
       this.dynamoPartitionKey = 'PK'
       this.dynamoSortKey = 'SK'
       this.cloudFrontGetUserPresenterDistributionDomain = 'https://d3q9q9q9q9q9q9.cloudfront.net'
     } else {
-      this.s3BucketName = env.S3_BUCKET_NAME as string
-      this.region = env.REGION as string
-      this.endpointUrl = env.ENDPOINT_URL as string
-      this.dynamoTableName = env.DYNAMO_TABLE_NAME as string
-      this.dynamoPartitionKey = env.DYNAMO_PARTITION_KEY as string
-      this.dynamoSortKey = env.DYNAMO_SORT_KEY as string
-      this.cloudFrontGetUserPresenterDistributionDomain = env.CLOUD_FRONT_DISTRIBUTION_DOMAIN as string
+      this.s3BucketName = process.env.S3_BUCKET_NAME as string
+      this.region = process.env.REGION as string
+      this.endpointUrl = process.env.ENDPOINT_URL as string
+      this.dynamoTableName = process.env.DYNAMO_TABLE_NAME as string
+      this.dynamoPartitionKey = process.env.DYNAMO_PARTITION_KEY as string
+      this.dynamoSortKey = process.env.DYNAMO_SORT_KEY as string
+      this.cloudFrontGetUserPresenterDistributionDomain = process.env.CLOUD_FRONT_DISTRIBUTION_DOMAIN as string
     }
   }
 
   static getUserRepo(): IUserRepository {
+    console.log('Environments.getEnvs().stage - [ENVIRONMENTS - { GET USER REPO }] - ', Environments.getEnvs().stage)
+
     if (Environments.getEnvs().stage === STAGE.TEST) {
       return new UserRepositoryMock()
-    } else if (Environments.getEnvs().stage in [STAGE.DEV, STAGE.PROD]) {
+    } else if (Environments.getEnvs().stage === STAGE.DEV || Environments.getEnvs().stage === STAGE.PROD) {
       return new UserRepositoryDynamo()
     } else {
       throw new Error('Invalid STAGE')
